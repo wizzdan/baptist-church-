f09c860f92080deb5f27a7d1d07e07c7303bebce
wizzdan <dansonnjihia0100@gmail.com>
2026-05-23 22:51:51 +0300

    Configure output file names in Vite build

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
+      }
+    }
+  }
 })
